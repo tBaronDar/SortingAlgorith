b5a97843ebe3e6d456747586b8ebf2ef7f85457f
tBaronDar <thedarelis@gmail.com>
2024-12-07 13:39:34 +0200

    Add project files.

---
+++ SortingAlgorith/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SortingAlgorith
+{
+    public  delegate int Compare<T>(T x,T y );
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
